53a89ab6ab200d58231c499c260c20b3a16f0a95
nanathar <lewalskimateusz94@gmail.com>
2022-10-28 23:04:40 +0200

    Add new project

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const div = document.querySelector('div.color')
+// Przyciski zmian koloru i stop Animacji
+const divResult = document.querySelector('div.result')
 
-div.addEventListener('click', () => {
-
-
+divResult.addEventListener('click', (div) => {
+  if (div.target.classList.contains('stop-start-animation')) {
+    document.querySelector('div.animation').classList.toggle('stop')
+  } else if (div.target.classList.contains('color')) {
+    document.body.classList.toggle('active');
+    divResult.classList.toggle('active')
+  }
 })
+
