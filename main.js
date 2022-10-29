db527fe76839c76993be429e5fac4688938c1a4a
nanathar <lewalskimateusz94@gmail.com>
2022-10-29 20:37:15 +0200

    Add new project

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+  }
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
+
+  delete() {
+
+  }
+  appendNumber(number) {
+    this.currentOperand = number;
+  }
+  chooseOperation(operation) {
+
+  }
+  computer() {
+
+  }
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
+  }
+
+}
+
+const numberDiv = document.querySelectorAll('.number');
+const operationDiv = document.querySelectorAll('.operator');
+const clearAll = document.querySelector('.clear-all');
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equals');
+const result = document.querySelector('.result');
+const currentOperandTextElement = document.querySelector('.current-operand');
+const previousOperandTextElement = document.querySelector('.previous-operand');
 
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
-
-
-
-const numberButtons = document.querySelectorAll
-
-
-
-
-
-
+numberDiv.forEach(div => {
+  div.addEventListener('click', () => {
+    calculator.appendNumber(div.innerText);
+    calculator.updateDisplay();
+  })
+})
 
 
 
